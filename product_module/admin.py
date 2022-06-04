5cff8c8d5be50722b1faa551e1a5feb49711201d
Suraj Shrestha <suraj.m.shrestha200@gmail.com>
2022-06-04 13:28:31 +0545

    Fifth Commit

--- product_module/admin.py
+++ product_module/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Brand, Category, Product
 admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(Product)
 
