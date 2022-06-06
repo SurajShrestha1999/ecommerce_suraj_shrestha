d831e5f402076b9d6549f7f92e03fdc8bd627abc
Suraj Shrestha <suraj.m.shrestha200@gmail.com>
2022-06-06 19:45:35 +0545

    Committed

--- product_module/admin.py
+++ product_module/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+
 from .models import Brand, Category, Product
 admin.site.register(Brand)
 admin.site.register(Category)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=["name","price","brand","category",]
+    search_fields=["name","price","brand__name","category__name",]
+    list_filter=["brand","category",]
+    #readonly_fields=["quantity",]
+
+    class Meta:
+        model=Product
+
+admin.site.register(Product,ProductAdmin)
+
 
